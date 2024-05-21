971e8a064f888d328e5ae5c86d5b578ab2aa946b
saurabhjb007 <saurabhsharma132000@gmail.com>
2024-05-21 15:28:58 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
   const [text, setText] = useState("");
   const {
     transcript,
-    listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
@@ -51,48 +50,50 @@ function App() {
   return (
     <>
       <ToastContainer position="top-right" theme="dark" autoClose={3000} />
-      <nav className="bg-gray-900 text-white font-bold p-5 w-full">
+      <nav className="bg-gray-800 text-white font-bold p-5 w-full">
         <div className="container mx-auto gap-3 flex items-center">
           <div className="text-3xl">Voice Recognition App </div>
-          <FaMicrophoneAlt size={30}/>
+          <FaMicrophoneAlt size={30} />
         </div>
       </nav>
-      <div className="h-[600px] flex flex-col justify-center items-center mt-10">
+      <div className="h-[600px] flex flex-col justify-center items-center mt-2 md:mt-10">
         <textarea
           value={transcript}
-          className="border-2 border-slate-600 text-2xl my-10 p-4"
+          className="border-2 border-slate-600 text-2xl my-10 p-4 h-[200px] w-3/4"
           onChange={handleOnChange}
           name="TextSearch"
           placeholder="Your Text Here"
           id="TextSearch"
-          cols="100"
-          rows="10"
         ></textarea>
-        <div className="flex gap-20">
-          <button
-            onClick={handleStartListening}
-            className="h-48 w-48 flex justify-center items-center bg-green-600 rounded-full"
-          >
-            <FaMicrophoneAlt size={70} />
-          </button>
-          <button
-            onClick={handleCopy}
-            className="h-48 w-48 flex justify-center items-center bg-blue-600 rounded-full"
-          >
-            <FaCopy size={70} />
-          </button>
-          <button
-            onClick={handleSearch}
-            className="h-48 w-48 flex justify-center items-center bg-red-600 rounded-full"
-          >
-            <IoSearch size={80} />
-          </button>
-          <button
-            onClick={handleReset}
-            className="h-48 w-48 flex justify-center items-center bg-red-600 rounded-full"
-          >
-            <GrPowerReset size={70} />
-          </button>
+        <div className="flex flex-col md:flex-row gap-10 xl:gap-20 my-5">
+          <div className="flex gap-10 xl:gap-20">
+            <button
+              onClick={handleStartListening}
+              className="h-24 w-24 md:h-36 md:w-36 xl:h-48 xl:w-48 flex justify-center items-center bg-green-600 rounded-full"
+            >
+              <FaMicrophoneAlt className="icons" />
+            </button>
+            <button
+              onClick={handleCopy}
+              className="h-24 w-24 md:h-36 md:w-36 xl:h-48 xl:w-48 flex justify-center items-center bg-blue-600 rounded-full"
+            >
+              <FaCopy className="icons" />
+            </button>
+          </div>
+          <div className="flex gap-10 xl:gap-20">
+            <button
+              onClick={handleSearch}
+              className="h-24 w-24 md:h-36 md:w-36 xl:h-48 xl:w-48 flex justify-center items-center bg-red-600 rounded-full"
+            >
+              <IoSearch className="icons" />
+            </button>
+            <button
+              onClick={handleReset}
+              className="h-24 w-24 md:h-36 md:w-36 xl:h-48 xl:w-48 flex justify-center items-center bg-red-600 rounded-full"
+            >
+              <GrPowerReset className="icons" />
+            </button>
+          </div>
         </div>
       </div>
     </>
