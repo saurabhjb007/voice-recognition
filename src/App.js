78d4725bbf0025f504d44b00dbf4043f2cdd327a
saurabhjb007 <saurabhsharma132000@gmail.com>
2024-05-21 14:30:49 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { FaCopy, FaMicrophoneAlt } from "react-icons/fa";
+import { GrPowerReset } from "react-icons/gr";
+import { IoSearch } from "react-icons/io5";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(transcript);
+    toast.success("Text copied to clipboard!");
+  };
+
+  const handleSearch = () => {
+    const query = encodeURIComponent(transcript);
+    const url = `https://www.google.com/search?q=${query}`;
+    window.open(url, "_blank");
+    toast.info("Search initiated!");
+  };
+
+  const handleStartListening = () => {
+    SpeechRecognition.startListening();
+    toast.info("Listening started!");
+  };
+
+  const handleReset = () => {
+    resetTranscript();
+    toast.warning("Transcript reset!");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer position="top-right" theme="dark" autoClose={3000} />
+      <nav className="bg-gray-900 text-white font-bold p-5 w-full">
+        <div className="container mx-auto gap-3 flex items-center">
+          <div className="text-3xl">Voice Recognition App </div>
+          <FaMicrophoneAlt size={30}/>
+        </div>
+      </nav>
+      <div className="h-[600px] flex flex-col justify-center items-center mt-10">
+        <textarea
+          value={transcript}
+          className="border-2 border-slate-600 text-2xl my-10 p-4"
+          onChange={handleOnChange}
+          name="TextSearch"
+          placeholder="Your Text Here"
+          id="TextSearch"
+          cols="100"
+          rows="10"
+        ></textarea>
+        <div className="flex gap-20">
+          <button
+            onClick={handleStartListening}
+            className="h-48 w-48 flex justify-center items-center bg-green-600 rounded-full"
+          >
+            <FaMicrophoneAlt size={70} />
+          </button>
+          <button
+            onClick={handleCopy}
+            className="h-48 w-48 flex justify-center items-center bg-blue-600 rounded-full"
+          >
+            <FaCopy size={70} />
+          </button>
+          <button
+            onClick={handleSearch}
+            className="h-48 w-48 flex justify-center items-center bg-red-600 rounded-full"
+          >
+            <IoSearch size={80} />
+          </button>
+          <button
+            onClick={handleReset}
+            className="h-48 w-48 flex justify-center items-center bg-red-600 rounded-full"
+          >
+            <GrPowerReset size={70} />
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
